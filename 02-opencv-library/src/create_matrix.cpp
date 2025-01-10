2a79138f0ed7d9d2cb270a78155c73ae4f7269eb
juliocastillo <julio.a.castillo.c@gmail.com>
2025-01-09 21:45:43 -0800

    feat: create matrix example

--- 02-opencv-library/src/create_matrix.cpp
+++ 02-opencv-library/src/create_matrix.cpp
@@ -13,10 +13,12 @@ void createMatrix1()
     m1_8u.create(nrows, ncols, CV_8UC1); // two-channel matrix with 8bit unsigned elements
     m1_8u.setTo(cv::Scalar(255));        // white
 
-    // STUDENT TASK :
+    // STUDENT TASK:
     // Create a variable of type cv::Mat* named m3_8u which has three channels with a
     // depth of 8bit per channel. Then, set the first channel to 255 and display the result.
-
+    cv::Mat m3_u8;
+    m3_u8.create(nrows, ncols, CV_8UC3); // three-channel matrix with 8bit unsigned elements
+    m3_u8.setTo(cv::Scalar(255, 0, 0));  // blue
 
     // show result
     string windowName = "First steps in OpenCV (m1_8u)";
@@ -24,9 +26,12 @@ void createMatrix1()
     cv::imshow(windowName, m1_8u);
     cv::waitKey(0); // wait for keyboard input before continuing
 
-    // STUDENT TASK :
+    // STUDENT TASK:
     // Display the results from the STUDENT TASK above
-
+    windowName = "First steps in OpenCV (m3_8u)";
+    cv::namedWindow(windowName, 1); // create window
+    cv::imshow(windowName, m3_u8);
+    cv::waitKey(0); // wait for keyboard input before continuing
 }
 
 
